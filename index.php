23222e0f256281edf1877f7cd1fdcbec06cb25d2
Eddinos <eddine.djerboua@gmail.com>
2014-08-29 17:16:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML >
+<html>
+  <head>
+		<title>RobotWithMe</title>
+		<link rel="stylesheet" href="css/style.css" charset="utf-8" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<LINK REL="SHORTCUT ICON" href="images/icon.ico">
+  </head>
+  <body>
+  
+  <?php include("banniere.php"); ?>
+  <?php include("carousel.php"); ?>
+  <?php include("footer.php"); ?>
+	</div>
+  </body>
+</html>
