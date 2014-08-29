e4aab4c2ec711d0d22b4a9e040e499e465898199
Eddinos <eddine.djerboua@gmail.com>
2014-08-29 17:13:22 +0200

    Create ciblecontact.php

---
+++ ciblecontact.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<html>
+<head>
+    <title>RobotWithMe</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css"/>
+	<LINK REL="SHORTCUT ICON" href="images/icon.ico">
+</head>
+
+
+<body>
+	<?php include("banniere.php"); ?>
+	<fieldset class="cible"><legend>Mail envoyé</legend>
+	<p> 
+	Votre message nous a bien été transmis et vous aurez une réponse dans les plus brefs délais !
+	<div class="backhome"><a href="index.php"> Retour à l'accueil </a></div>
+	</p>
+	</fieldset>
+	<?php include("footer.php"); ?>	
+	<script src="js/valider.js" ></script>
+	</body>
+</html>
